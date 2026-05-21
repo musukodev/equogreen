c01be3fd922024996596f5d72b400600c4e333e7
unknown <rafisiregar1721@gmail.com>
2026-05-21 15:42:18 +0700

    Refresh git cache

---
+++ resources/views/emails/vendor-approved.blade.php
@@ -0,0 +1,9 @@
+<h2>Vendor Approved</h2>
+
+<p>Halo {{ $vendor->nama_perusahaan }},</p>
+
+<p>Akun Anda sudah divalidasi dan terdaftar di Equogreen.</p>
+
+<p>Silahkan Reset password anda <a href="{{ route('password.request') }}">di sini</a>.</p>
+
+<p>Terima kasih.</p>    
